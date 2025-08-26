d00ccfb93add5aae44fbad3e3737f3bd68387cd6
yoshikouki <yoshikouki@pepabo.com>
2025-08-26 22:13:21 +0900

    Setup TailwindCSS

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ const app = new Hono();
 app.use(renderer);
 
 app.get("/", (c) => {
-  return c.render(<h1>Hello!</h1>);
+  return c.render(<h1 className="text-3xl font-bold underline">Hello!</h1>);
 });
 
 export default app;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import ssrPlugin from "vite-ssr-components/plugin";
 
 export default defineConfig({
-  plugins: [cloudflare(), ssrPlugin()],
+  plugins: [cloudflare(), ssrPlugin(), tailwindcss()],
 });
